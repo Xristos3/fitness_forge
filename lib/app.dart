d6769836fece9f0157fe375610229cc0e5583745
Nicos <hfnovember@hotmail.com>
2023-06-12 16:04:38 +0300

    Created project structure

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:fitness_forge/ui/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class FitnessForgeApp extends StatelessWidget {
+  const FitnessForgeApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitness Forge app',
+      home: const HomeScreen(),
+    );
+  }
+}
