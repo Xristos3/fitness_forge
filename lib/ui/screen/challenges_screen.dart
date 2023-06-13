703b91c0827abebf51da5fa8d251562b582b82fe
Xristos3 <xristossavvides1@gmail.com>
2023-06-13 15:10:46 +0300

    screens and images updated

--- lib/ui/screen/challenges_screen.dart
+++ lib/ui/screen/challenges_screen.dart
@@ -1,10 +1,99 @@
+import 'package:fitness_forge/ui/screen/achievements_screen.dart';
+import 'package:fitness_forge/ui/screen/badges_profile_screen.dart';
+import 'package:fitness_forge/ui/screen/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 class ChallengesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Challenges'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Challenge'),
+      ),
+      body: ListView(
+        children: [
+          CustomRightAlignedContainer(
+            title: 'Outside Activities',
+            image: 'images/walk.png',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AchievementsScreen()),
+              );
+            },
+          ),
+          CustomRightAlignedContainer(
+            title: 'Homebased Activities',
+            image: 'images/home.png',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ChatScreen()),
+              );
+            },
+          ),
+          CustomRightAlignedContainer(
+            title: 'Seasonal Activities',
+            image: 'images/season.jpeg',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => BadgesScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomRightAlignedContainer extends StatelessWidget {
+  final String title;
+  final String image;
+  final VoidCallback onPressed;
+
+  const CustomRightAlignedContainer({
+    required this.title,
+    required this.image,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                ElevatedButton(
+                  onPressed: onPressed,
+                  child: Text('Select'),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 16.0),
+          Image.asset(
+            image,
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
     );
   }
 }
