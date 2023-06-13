254b3bab3d1be8b368631fde9a766c5b4537d5db
Xristos3 <xristossavvides1@gmail.com>
2023-06-13 14:57:44 +0300

    chat screen updated

--- lib/ui/screen/chat_screen.dart
+++ lib/ui/screen/chat_screen.dart
@@ -3,8 +3,70 @@ import 'package:flutter/material.dart';
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Chat'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat Screen'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                // Messages will be displayed here
+                ChatMessage(text: 'Hello!', isSentByMe: true),
+                ChatMessage(text: 'Hi there!', isSentByMe: false),
+                ChatMessage(text: 'How are you?', isSentByMe: true),
+                ChatMessage(text: 'I\'m doing great!', isSentByMe: false),
+                // Add more ChatMessage widgets for more messages
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(hintText: 'Type a message...'),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    // Send button functionality
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final bool isSentByMe;
+
+  ChatMessage({required this.text, required this.isSentByMe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: isSentByMe ? Alignment.centerRight : Alignment.centerLeft,
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Container(
+        padding: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          color: isSentByMe ? Colors.blue : Colors.grey,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
