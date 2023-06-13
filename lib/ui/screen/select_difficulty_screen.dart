42d4bb4fa8b62b07091bbb153959d51b6c925c8e
Xristos3 <xristossavvides1@gmail.com>
2023-06-13 11:08:23 +0300

    workout screens added and images

---
+++ lib/ui/screen/select_difficulty_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class SelectDifficultyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Difficulty'),
+      ),
+      body: ListView(
+        children: [
+          CustomRightAlignedContainer(
+            title: 'Easy',
+            description: 'Composed of basic callisthenic exercises to help you start your fitness journey ',
+            image: 'images/custom.jpeg',
+          ),
+          CustomRightAlignedContainer(
+            title: 'Medium',
+            description: 'Composed of more advanced versions of basic callisthenic exercises ',
+            image: 'images/custom.jpeg',
+          ),
+          CustomRightAlignedContainer(
+            title: 'Hard',
+            description: 'Test your limits with exercises that will push your muscles to new levels',
+            image: 'images/custom.jpeg',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomRightAlignedContainer extends StatelessWidget {
+  final String title;
+  final String description;
+  final String image;
+
+  const CustomRightAlignedContainer({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  description,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 16.0),
+          Image.asset(
+            image,
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+}
