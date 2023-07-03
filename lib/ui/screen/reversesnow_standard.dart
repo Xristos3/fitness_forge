e4360d2b358c158e6b68944f5c5f2623faa61d52
Xristos3 <xristossavvides1@gmail.com>
2023-07-03 13:54:12 +0300

    standard upper workout done

---
+++ lib/ui/screen/reversesnow_standard.dart
@@ -0,0 +1,115 @@
+import 'dart:async';
+import 'package:fitness_forge/ui/screen/noseandtoes_standard.dart';
+import 'package:flutter/material.dart';
+
+class ReverseSnowAngelsScreenStandard extends StatefulWidget {
+  @override
+  _PushUpUpperScreenStandardState createState() =>
+      _PushUpUpperScreenStandardState();
+}
+
+class _PushUpUpperScreenStandardState extends State<ReverseSnowAngelsScreenStandard> {
+  int countdown = 30;
+  bool isCountdownActive = false;
+
+  void startCountdown() {
+    if (!isCountdownActive) {
+      setState(() {
+        countdown = 30;
+        isCountdownActive = true;
+      });
+
+      const oneSec = const Duration(seconds: 1);
+      Timer.periodic(oneSec, (Timer timer) {
+        if (countdown == 0) {
+          setState(() {
+            isCountdownActive = false;
+          });
+          timer.cancel();
+        } else {
+          setState(() {
+            countdown--;
+          });
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Screen'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Reverse snow angels',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Lie down on the floor on your chest and stomach. '
+                    'Stretch your hands forward and your legs at the back. '
+                    'The palms should face the floor and your toes should be pointed towards the floor. '
+                    'Lift your arms and legs so that they hover across the floor.',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: 16.0),
+              Image.asset(
+                'images/rsas.png', // Replace with the actual path to your image
+                width: 200.0,
+                height: 200.0,
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                child: Text('Next'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => NoseAndToesScreenStandard(),
+                    ),
+                  );
+                },
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Sets: 4 sets Reps: Until Failure for each set.',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                child: Text(
+                  'Take a break from each set',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: isCountdownActive ? null : startCountdown,
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Countdown: $countdown seconds',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
