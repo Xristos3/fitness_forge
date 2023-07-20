6cb16b035edb803c88f9acaa76ebf2529f4214ce
Xristos3 <xristossavvides1@gmail.com>
2023-07-20 06:48:49 +0300

    challenge timers

--- lib/ui/screen/friendslist_screen.dart
+++ lib/ui/screen/friendslist_screen.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitness_forge/ui/screen/friendbadges2_profile_screen.dart';
 import 'package:fitness_forge/ui/screen/friendrequest_screen.dart';
-import 'package:fitness_forge/ui/screen/friendsbadges_profile_screen.dart';
 import 'package:flutter/material.dart';
 
 class FriendsListScreen extends StatefulWidget {
